dcc9e9e78680b0d3d311ca09aae71af67fd70ea4
ZarhanMemon <ironmanred118@gmail.com>
2024-07-25 13:25:23 +0530

    Update Project-1 [GAME].py

--- Project-1 [GAME].py
+++ Project-1 [GAME].py
@@ -3,25 +3,25 @@ import random
  
 def game(player,comp):
     if ( player =="r" or player =="rock") and comp=="rock":
-        print("The match is a tie !")
+        print("\nThe match is a tie !")
     elif ( player =="r" or player =="rock") and comp=="paper":
-        print("Computer has WON!")
+        print("\nComputer has WON!")
     elif ( player =="r" or player =="rock") and comp=="scisor":
-        print("Player has WON!")
+        print("\nPlayer has WON!")
       
     elif ( player =="p" or player =="paper") and comp=="rock":
-        print("Player has WON!")
+        print("\nPlayer has WON!")
     elif  ( player =="p" or player =="paper") and comp=="paper":
-        print("The match is a tie !")
+        print("\nThe match is a tie !")
     elif ( player =="p" or player =="paper") and comp=="scisor":
-        print("Computer has WON!")   
+        print("\nComputer has WON!")   
    
     elif  ( player =="s" or player =="scisor") and comp=="rock":
-        print("Computer has WON!")
+        print("\nComputer has WON!")
     elif  ( player =="s" or player =="scisor") and comp=="paper":
-        print("Player has WON!")
+        print("\nPlayer has WON!")
     elif  ( player =="s" or player =="scisor") and comp=="scisor":
-        print("The match is a tie !") 
+        print("\nThe match is a tie !") 
         
     else:
         return print("Invalid")  
@@ -36,12 +36,14 @@ while 0==0:
   elif randomno==3:
     comp="scisor"
 
-
-  print("Comp Enter : Rock , Paper or Scisor ?")
+  print("\n       ====GAME====  ")
+  
+  print("\nComp Enter : Rock , Paper or Scisor ?")
   player = input("Player Enter : Rock(r), Paper(p) or Scisor(s) :")
-
-  print(f"Player turn :{player.capitalize()}")
+  
+  print(f"\nPlayer turn :{player.capitalize()}")
   print(f"Comp turn :{comp.capitalize()}")
+  
 
 
   game(player,comp)
@@ -50,8 +52,8 @@ while 0==0:
 
   play_again= input("\nDo you want to play again? (yes/no):").strip().lower()  #strip is used to cut the space and lower to print next to line
 
-  if play_again != "yes":
-   break  
+  if play_again == "no":
+    break  
 
 print("Thanks for playing!")
 
