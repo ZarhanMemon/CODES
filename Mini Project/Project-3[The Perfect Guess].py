bdac544562644d3e80f75ffa607c72cea7d3c9a1
ZarhanMemon <ironmanred118@gmail.com>
2024-09-13 11:25:06 +0530

    f

--- Mini Project/Project-3[The Perfect Guess].py
+++ Mini Project/Project-3[The Perfect Guess].py
@@ -16,16 +16,16 @@ def Guess():
    
  while (ply_choice != random_no):
     
-   ply_choice = int(input("\nEnter your guess no:"))
+   ply_choice = int(input("\nEnter your guess no: "))
   
    guess +=1
   
    if (ply_choice == random_no):
-     print(f"U have Guessed Right {ply}!..") 
+     print(f"U have Guessed Right {ply}!..\n") 
      print(f"You guessed the no in {guess} guesses")
 
    else:
-    print(f"Your guessed it wrong..")
+    print(f"Your guessed is wrong..")
     if (ply_choice > random_no): 
       print(f"\nHINT : Guess is high, guess low no plz**\n")
     elif (ply_choice < random_no): 
@@ -34,19 +34,20 @@ def Guess():
         print("INVALID")
       
    lives -= 1
-   if lives>0:
+   if lives>0 and guess!=random_no:
     print(f"U have {lives} lives left..Play save\n")
     print("-------------------------------------")
-   elif lives ==0:
-      print("\nYou Lost the Game ")
+   elif lives==0 and guess!=random_no:
+      print("\n======You Lost the Game======")
       break
    
-  
- play_again= input("\nDo you want to play again? (y/n):").strip().lower()  #strip is used to cut the space and lower to print next to line
- while 0==0:
+ if True :
+  play_again= input("\nDo you want to play again? (y/n):").strip().lower()  #strip is used to cut the space and lower to print next to line
+  while 0==0:
      if play_again == "n":
        break  
-    
+  else:
+      print("INVALID")
       
  print("\n Thanks for playing!")
 
