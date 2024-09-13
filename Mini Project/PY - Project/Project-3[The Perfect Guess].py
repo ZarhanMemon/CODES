e6741dee1f6e3c4c8c23e92efda30caf5987b154
ZarhanMemon <ironmanred118@gmail.com>
2024-09-13 13:26:38 +0530

    a

--- Mini Project/PY - Project/Project-3[The Perfect Guess].py
+++ Mini Project/PY - Project/Project-3[The Perfect Guess].py
@@ -1,55 +1,57 @@
 import random
 
-print("\n       ====The-Perfect-Guess====  ")
-  
-ply = input("\nEnter yur name? :")
+ply_name = "Zarhan"
+print("\n      ^^^^^$====The-Perfect-Guess====$^^^^^      \n")
+print(f"Welcome! {ply_name} to Perfect Guess__\n")  
 
+  
 
 def Guess():
-   
+  
  random_no = random.randint(1,100)
-#  print(random_no)
+ #print(random_no)
     
  ply_choice=None
  guess =0
  lives = 5
    
  while (ply_choice != random_no):
-    
-   ply_choice = int(input("\nEnter your guess no: "))
-  
-   guess +=1
+   if guess==0:  
+    ply_choice = int(input("\nEnter your guess any no from 1 to 100: "))
+    guess +=1
+   else:
+    ply_choice = int(input("\nEnter your guess no : "))
   
    if (ply_choice == random_no):
-     print(f"U have Guessed Right {ply}!..\n") 
-     print(f"You guessed the no in {guess} guesses")
+     print(f"U have Guessed Right {ply_name}!..\n") 
+     print(f"You guessed the no in {guess} guesses\n")
 
    else:
-    print(f"Your guessed is wrong..")
+    print(f"Your guessed it wrong***")
     if (ply_choice > random_no): 
-      print(f"\nHINT : Guess is high, guess low no plz**\n")
+      print(f"\nHINT : Guess is high, guess low no plz..\n")
     elif (ply_choice < random_no): 
-      print(f"\nHINT : Guess is low, guess high no plz**\n")
+      print(f"\nHINT : Guess is low, guess high no plz..\n")
     else:
         print("INVALID")
       
    lives -= 1
    if lives>0 and guess!=random_no:
     print(f"U have {lives} lives left..Play save\n")
-    print("-------------------------------------")
-   elif lives==0 and guess!=random_no:
+    print("----------------------------------------")
+   else:
       print("\n======You Lost the Game======")
-      break
-   
- if True :
-  play_again= input("\nDo you want to play again? (y/n):").strip().lower()  #strip is used to cut the space and lower to print next to line
-  while 0==0:
-     if play_again == "n":
-       break  
-  else:
-      print("INVALID")
-      
- print("\n Thanks for playing!")
+      print(f"The correct number was {random_no}.")
 
 
-Guess()
+
+def main():
+    while True:
+        Guess()
+        play_again = input("\nDo you want to play again? (y/n): ").strip().lower()
+        if play_again != 'y':
+            print("\nThanks for playing!")
+            break
+          
+          
+main()
