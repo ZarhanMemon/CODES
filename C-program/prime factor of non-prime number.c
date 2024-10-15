00645f354851c206adb408b04eeffa7c2432f410
ZarhanMemon <ironmanred118@gmail.com>
2024-10-15 14:57:06 +0530

    Update prime factor of non-prime number.c

--- C-program/prime factor of non-prime number.c
+++ C-program/prime factor of non-prime number.c
@@ -1,57 +1,43 @@
 
 #include <stdio.h>
-int main()
-{
-    
-int num,i;
-int is_prime = 1;
 
+int main() {
+    int num, i;
+    int is_prime = 1;
 
+    printf("Enter the number for checking its prime number: ");
+    scanf("%d", &num);
 
-printf("Enter the number for checking its prime number?:");
-scanf("%d", &num);
-
-if (num<=1)
-{
-    is_prime=0;
-}
-
-else{
-    
-    for (i=2;i*i<=num;i++)
-    {
-        
-        if (num % i == 0)
-        {
-            is_prime=0;
-            break;
+    if (num <= 1) {
+        is_prime = 0;
+    } else {
+        for (i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                is_prime = 0;
+                break;
+            }
         }
     }
-}
-if (is_prime){
-    printf("\n The number %d is a prime number \n",num);
-     
-}
-else{
-    printf("\n The number %d is not a prime number",num);
-}    
 
-int factor = 2;
+    if (is_prime) {
+        printf("\nThe number %d is a prime number.\n", num);
+    } else {
+        printf("\nThe number %d is not a prime number.\n", num);
+        
+        int factor = 2; // Start with the first prime number
+        printf("The prime factors of %d are:", num);
 
-if (~is_prime){
-printf("\nThe prime factors of %d are :",num);
-     
-     while (num>1){
-         if (num % factor ==0){
-             
-             printf(" %d,", factor);
-             num = num / factor;
-         }
-         else{
-             factor++;
-         }
-     }
-}    
-    
+        // Check for prime factors
+        while (num > 1) {
+            if (num % factor == 0) {
+                printf(" %d", factor);
+                num = num / factor;
+            } else {
+                factor++;
+            }
+        }
+        printf("\n"); // Newline for better formatting
+    }
+
     return 0;
 }
