54b5a4ed413333892ee51cc4d0619826754ef264
ZarhanMemon <ironmanred118@gmail.com>
2024-10-22 15:09:54 +0530

    Create fibonacci.c

---
+++ C-program/fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+
+int n,count=0;
+
+unsigned long first=0,second=1,next;
+
+printf("Enter the number (positive num) of fibonacci no to be generated: ");
+scanf("%d",&n);
+
+printf("\n Fibonacci series :");
+
+while (count<n){
+
+ next = first + second;
+ printf("%lu",first);
+ first=second;
+ second = next ;
+ count++;
+}
+
+return 0;
+}
