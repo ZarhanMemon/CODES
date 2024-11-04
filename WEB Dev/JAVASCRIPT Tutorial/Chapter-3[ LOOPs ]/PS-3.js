2934cf9fff027fcabf617655e1983455ea1a5a60
ZarhanMemon <ironmanred118@gmail.com>
2024-11-04 22:37:41 +0530

    Update PS-3.js

--- WEB Dev/JAVASCRIPT Tutorial/Chapter-3[ LOOPs ]/PS-3.js
+++ WEB Dev/JAVASCRIPT Tutorial/Chapter-3[ LOOPs ]/PS-3.js
@@ -39,3 +39,72 @@ let  values = obj[keys];
 
 console.log(keys + " = " +values);
 }
+
+
+
+// Q1
+
+
+for (a = 0; a <=10; a++) {
+    console.log(a);
+    
+}
+console.log()
+
+
+
+// Q3
+
+
+ let number = [5 ,10 ,15 ,20,25];
+for (const keys of number) {
+    console.log(keys );
+ 
+}
+console.log()
+
+
+
+//Q4
+
+
+let string  = "hello";
+for (let i = string.length-1; i >= 0; i--) {       // make h e l l o ==>  o l l e h  By changing condition and i value
+   
+   process.stdout.write(string[i]);               // Print without a newline
+  
+}
+
+console.log()
+console.log()
+
+
+
+
+//Q5
+
+
+let x = 2;
+for (let num = 1; num <= 5; num++) {
+
+    if (num % x == 0 && num !=2) {
+
+        console.log(num + " is not prime\n")
+    }
+    else {
+        
+        if (num % x != 0) {
+           
+            console.log(num + " is a prime\n")
+        }
+        else{
+            if (num==2){
+                console.log(num + " is a prime\n")
+            }
+        }
+    }
+
+}
+console.log()
+
+
