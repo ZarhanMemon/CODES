60abb3e3b893a9744686df14663d0a48578936ac
ZarhanMemon <ironmanred118@gmail.com>
2024-11-06 16:21:59 +0530

    Create ch-5.js

---
+++ WEB Dev/JAVASCRIPT Tutorial/Chapter-5[ String ]/ch-5.js
@@ -0,0 +1,32 @@
+console.log("This is strings tutorial")
+let a = "Harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]); --> undefined 
+
+console.log(a.length)
+
+let real_name = "Harry"
+let friend = "Rohan"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b = "ShivamSh"
+
+// PROPERTIES OF STRING
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1))
+console.log(b.startswith("S")
+console.log(b.startswith("s")
+console.log(b.endswith("h")
+
+console.log(b.replace("Sh", "77")) // it only replaces first Sh not last Sh
+console.log(b.concat(a, "Aishwariya", "Rahul", "Priya"))   // it works same like--> C.L( "djwnd" + "dwjnd")
+
+console.log(b)    // string is immunable we cant permanently change string indirectly
