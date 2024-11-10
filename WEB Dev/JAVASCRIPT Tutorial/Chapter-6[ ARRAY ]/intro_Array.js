dd414bb7af77784fdd55b65c015517aad58338f9
ZarhanMemon <ironmanred118@gmail.com>
2024-11-10 20:38:01 +0530

    Create intro_Array.js

---
+++ WEB Dev/JAVASCRIPT Tutorial/Chapter-6[ ARRAY ]/intro_Array.js
@@ -0,0 +1,21 @@
+let arr = [1, 2, 4, 5, 7]
+//  Index  0, 1, 2, 3, 4
+
+console.log(arr)
+
+console.log(arr[0])  //=> 1
+console.log(arr[2])
+console.log(arr[4])
+
+arr[0] = 5666;
+
+console.log(arr); // array are mutable in js means changable
+
+console.log(arr[0]) // => 5666
+console.log(arr[2])
+console.log(arr[4])
+
+
+console.log(typeof arr) //--> type of array is OBJECT in js
+
+console.log(arr.length) //--> length of array is 5
