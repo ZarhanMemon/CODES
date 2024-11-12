c9eabd63d5f22f366753c4bd1bf95cb139c12c08
ZarhanMemon <ironmanred118@gmail.com>
2024-11-12 21:51:03 +0530

    Update Ex-11.js

--- WEB Dev/JAVASCRIPT Tutorial/Exercise /Ex-11.js
+++ WEB Dev/JAVASCRIPT Tutorial/Exercise /Ex-11.js
@@ -1,38 +1,40 @@
+// Calculate factorial using reduce and for loop
 
-// Write a program to calculate factorial of a number using reduce and using for loops
 
-
-// 6! = 6*5*4*3*2*1
-
- 
- 
+// Factorial using reduce
 let factorial_1 = (n) => {
 
+    // Generate array from 1 to n
     const array_factorial = (n) => {
         let a = [];
         for (let i = 1; i <= n; i++) {
-            a.unshift(i)
+            a.unshift(i)  // Add numbers in reverse order
         }
-        return a;
+        return a;  // Return the array
     }
 
+    // Calculate factorial using reduce
     let a = array_factorial(n);
     let result = a.reduce((acc, crr) => acc * crr, 1);
+    
+    // Output result
     console.log("\nThe " + a[0] + "! factorial is " + result + ".\n");
 }
 
+// Call factorial_1 for 6
 factorial_1(6);
 
 
 
-
+// Factorial using for loop
 let factorial_2 = (n) => {
-    let result = 1;
+    let result = 1;  // Start with result = 1
     for (let i = n; i > 0; i--) {
-        result *= i;
+        result *= i;  // Multiply result by i
     }
-    console.log("The " + n + "! factorial is " + result + ".");;
+    // Output result
+    console.log("The " + n + "! factorial is " + result + ".");
 }
 
-
+// Call factorial_2 for 6
 factorial_2(6);
