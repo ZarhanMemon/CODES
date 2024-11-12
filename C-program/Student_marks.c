c1860b608a6037136e58d26ef3c7ef37cdae82cd
ZarhanMemon <ironmanred118@gmail.com>
2024-11-12 14:17:38 +0530

    Create Student_marks.c

---
+++ C-program/Student_marks.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    float marks[5];
+    float total = 0.0, aggregate;
+    int i;
+    int pass = 1;
+    
+    printf("Enter the marks of five subjects out of 100:\n");
+    
+ 
+    for (i = 0; i < 5; i++) {
+        printf("Subject %d: ", i + 1);
+        scanf("%f", &marks[i]);
+        
+      
+        if (marks[i] <= 40) {
+            pass = 0;
+        }
+        
+        total += marks[i];
+    }
+    
+    
+    aggregate = total / 5;
+    
+  
+    if (!pass) {
+        printf("\n Result: Fail\n\n");
+    } else {
+        printf("Result: Pass\n\n");
+        printf("Aggregate Percentage: %.2f%% \n\n", aggregate);  
+        
+ 
+        if (aggregate >= 75) {
+            printf("Grade: Distinguished\n");
+        } else if (aggregate >= 60) {
+            printf("Grade: First Division\n");
+        } else if (aggregate >= 50) {
+            printf("Grade: Second Division\n");
+        } else if (aggregate >= 40) {
+            printf("Grade: Third Division\n");
+        }
+    }
+
+    return 0;
+}
