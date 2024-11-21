96387153be835ddb2050feca64fb8c753e067ba4
ZarhanMemon <ironmanred118@gmail.com>
2024-11-21 19:26:07 +0530

    Update Promises & its_Chain.js

--- WEB Dev/JAVASCRIPT Tutorial/Chapter-8[ CallBacks & Promises ]/Promises & its_Chain.js
+++ WEB Dev/JAVASCRIPT Tutorial/Chapter-8[ CallBacks & Promises ]/Promises & its_Chain.js
@@ -104,3 +104,84 @@ q.then(()=>{ console.log("1") });
 q.then(()=>{ console.log("2") });
 
 q.then(()=>{ console.log("3") });
+
+
+
+//--------------------------------------------------------------------------------------------------
+
+
+
+let q = new Promise((resolve, reject) => {
+    resolve("message1");
+})
+
+let p = new Promise((resolve, reject) => {
+    reject("message2");
+ })
+
+let a = new Promise((resolve, reject) => {
+    resolve("message3");
+})
+
+let b = new Promise((resolve, reject) => {
+    reject("message4");
+ })
+
+
+//     METHODS OF PROMISSES :-
+
+
+// Promise.all(): Resolves when all promises resolve. Rejects if any promise rejects.
+
+// Promise.allSettled(): Resolves when all promises settle (either resolved or rejected).
+
+// Promise.race(): Resolves or rejects as soon as the first promise resolves or rejects.
+
+// Promise.resolve(): Returns a resolved promise with a given value.
+
+// Promise.reject(): Returns a rejected promise with a given reason.
+
+
+
+
+let r = Promise.all([q , p , a , b]);     //it only prints the resolved ones if one is rejected then its not printed
+
+r.then((res)=>{
+    console.log(res)
+}).catch(()=>{
+    console.log("Error q/p/a/b might one throw error");
+})
+
+
+
+
+let s = Promise.allSettled([q , p , a , b]);   //it prinst either q,p,a,b are resolved or not
+
+s.then((res)=>{
+    console.log(res)
+})
+
+
+
+let promise1 = new Promise((resolve) => setTimeout(resolve, 500, 'First'));
+let promise2 = new Promise((reject) => setTimeout(reject, 100, 'Second'));
+
+let t = Promise.race([promise1, promise2]);
+   t.then((result) => {
+        console.log(result);           // 'Second' (because promise2 resolves first it might be reject then it also print same)
+    });
+
+
+
+let u = Promise.resolve(42);
+u.then(value => {
+    console.log(value);      // 42
+});
+
+
+
+let v = Promise.reject('Something went wrong!');
+v.catch(err => {
+    console.log(err);  // 'Something went wrong!'
+});
+
