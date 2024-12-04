4b44bfeb248a5be39834301ad46f551128b850c2
ZarhanMemon <ironmanred118@gmail.com>
2024-12-04 14:34:24 +0530

    Update and rename Salary,hra,da.c to Experiment_No-1.c

---
+++ C-program/Experiment_No-1.c
@@ -0,0 +1,31 @@
+ 
+//Experiment No - 1
+//Roll no - 35
+//Div - A
+
+#include <stdio.h>
+int main() {
+
+int basic;
+int hra,da,gross_salary;
+
+
+printf("Enter your salary:");
+scanf("%d",&basic);
+
+hra = 0.2*basic;
+da = 1.5*basic;
+gross_salary = basic + hra + da;
+
+
+printf("\nThe gross salary for %d$ basic, %d$ da and %d$ hra is %d$.",basic,da,hra,gross_salary);
+
+return 0;
+}
+
+// Output
+
+//Enter your salary:30000
+
+// The gross salary for 30000$ basic, 45000$ da and 6000$ hra is 81000$.
+
