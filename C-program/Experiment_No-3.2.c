9c76c819d51b41819d9805cc3210dd04455afc88
ZarhanMemon <ironmanred118@gmail.com>
2024-12-04 14:47:47 +0530

    Update Experiment_No-3.2.c

--- C-program/Experiment_No-3.2.c
+++ C-program/Experiment_No-3.2.c
@@ -3,29 +3,17 @@
 //Div - A
 
 #include <stdio.h>
-
  int main() {
-    
-    int a,b;   // a --> input no and b is output of a²
-    
+    int a,b;   
     printf("Enter the number to square:");
-   
-        
-    scanf("%d",&a); //substitute a as %d used in string 
-    
-      b = a*a;    //maths
-      
+    scanf("%d",&a); 
+      b = a*a;    
     printf("\n %d = %d²",b,a);
-    
-    printf("\n The result of square %d is %d",a,b);   //output and substitution 
-                    // of a,b in string
-
+    printf("\n The result of square %d is %d",a,b);   
     return 0;
 }
 
 //OUTPUT
-
 //Enter the number to square:3
-
 //9 = 3²
 //The result of square 3 is 9
