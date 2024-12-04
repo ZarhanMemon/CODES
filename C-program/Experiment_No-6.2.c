a224d7971d3a73d271f5360188c8254350a141b3
ZarhanMemon <ironmanred118@gmail.com>
2024-12-04 16:52:21 +0530

    Update C-program

---
+++ C-program/Experiment_No-6.2.c
@@ -0,0 +1,46 @@
+//Experiment No - 6.2
+//Roll no - 35
+//Div - A
+
+
+#include <stdio.h>
+
+void findMaxElement(int arr[], int n) {
+    int maxElement = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];
+        }
+    }
+
+    printf("Maximum element: %d\n", maxElement);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    findMaxElement(arr, n);
+
+    return 0;
+}
+
+
+//Output 
+
+//Enter the number of elements: 4
+//Enter 4 elements:
+//1
+//2
+//3
+//4
+//Maximum element: 4
+
