4e3c83de28e9dfae96e20195fe7c83fe13549e5a
ZarhanMemon <ironmanred118@gmail.com>
2024-12-04 17:04:53 +0530

    Update C-program

---
+++ C-program/Experiment_No-6.4.c
@@ -0,0 +1,49 @@
+//Experiment No - 6.4
+//Roll no - 35
+//Div - A
+
+
+#include <stdio.h>
+
+void findAverage(int arr[], int n) {
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    float average = (float)sum / n;
+    printf("Average of elements: %.2f\n", average);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    findAverage(arr, n);
+
+    return 0;
+}
+
+
+
+//Output 
+
+//Enter the number of elements: 4
+//Enter 4 elements:
+//3
+//4
+//2
+//1
+
+//Average of elements: 2.50
+
+
+
